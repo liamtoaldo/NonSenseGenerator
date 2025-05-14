c748984afb6fa6109a1b126d96f0b2cdf2059746
jurimasonStudent <juri.mason@barsanti.edu.it>
2025-05-14 21:47:52 +0200

    NEW - springboot project

---
+++ nonsense/src/main/java/com/swe/nonsense/NonSenseGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.swe.nonsense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonSenseGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonSenseGeneratorApplication.class, args);
+	}
+
+}
---
+++ nonsense/src/test/java/com/swe/nonsense/NonSenseGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swe.nonsense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonSenseGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
