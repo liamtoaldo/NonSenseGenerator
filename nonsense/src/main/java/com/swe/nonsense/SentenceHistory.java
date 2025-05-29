60a29154fc81fefb31ae128088768fe5f7bbadf7
Pleui <pietro.pleui.nicolini@gmail.com>
2025-05-29 23:13:04 +0200

    NEW - Aggiunta la classe SentenceHistory

---
+++ nonsense/src/main/java/com/swe/nonsense/SentenceHistory.java
@@ -0,0 +1,39 @@
+package com.swe.nonsense;
+
+import java.util.ArrayList;
+
+public class SentenceHistory {
+    private static SentenceHistory instance;
+    private ArrayList<Sentence> savedSentences;
+
+    //Constructors
+    private SentenceHistory() {
+        if (instance != null) {
+            throw new IllegalStateException("SentenceHistory is a singleton and cannot be instantiated multiple times");
+        }
+        this.savedSentences = new ArrayList<Sentence>();
+    }
+
+    private SentenceHistory(ArrayList<Sentence> savedSentences) {
+        if (instance != null) {
+            throw new IllegalStateException("SentenceHistory is a singleton and cannot be instantiated multiple times");
+        }
+        this.savedSentences = savedSentences;
+    }
+
+    public static SentenceHistory getInstance() {
+        return instance;
+    }
+
+    public void save(Sentence sentence) {
+        savedSentences.add(sentence);
+    }
+
+    public ArrayList<Sentence> getLastSentence(int numberOfSentences) {
+        ArrayList<Sentence> lastSentences = new ArrayList<>();
+        for (int i = 0; i < numberOfSentences; i++) {
+            lastSentences.add(savedSentences.get((savedSentences.size() - 1) - i));
+        }
+        return lastSentences;
+    }
+}
