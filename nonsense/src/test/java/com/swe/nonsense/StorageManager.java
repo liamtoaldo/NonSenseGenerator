a9bb40f610c41cbe42ab78a16267d97c9e2d6787
jurimasonStudent <juri.mason@barsanti.edu.it>
2025-05-30 16:51:49 +0200

    NEW - Mock classe StorageManager per build in dev del progetto

---
+++ nonsense/src/test/java/com/swe/nonsense/StorageManager.java
@@ -0,0 +1,9 @@
+package com.swe.nonsense;
+
+public class StorageManager {
+    public WordsDictionary loadDictionary() {
+        // This method should return a new instance of WordsDictionary
+        // For the sake of this example, we will return a new instance directly.
+        return null;
+    }
+}
