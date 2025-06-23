45b271310303bcf40d52d82a9401a06a416676dd
Pleui <pietro.pleui.nicolini@gmail.com>
2025-06-23 16:16:18 +0200

    EDIT - Ultime modifiche ai commenti Javadoc

--- nonsense/src/main/java/com/swe/nonsense/ErrorRestController.java
+++ nonsense/src/main/java/com/swe/nonsense/ErrorRestController.java
@@ -4,18 +4,19 @@ import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
 /**
  * REST controller that handles errors in the application controller
  */
+@RestController
 public class ErrorRestController implements ErrorController {
-
-    @RequestMapping("/error")
+    
     /**
      * Main method of the class that handles errors in the application
      * 
      * @return A String message indicating that an error has occurred
      */
+    @RequestMapping("/error")
     public String handleError() {
         return "Something went wrong. Please try again later.";
     }
